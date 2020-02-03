9309f7f08d9239e0e9e3565b0732d90e582b0286
Alessandro Menezes <alessandroadm@live.com>
2020-02-03 17:06:59 -0300

    Adicionando o arquivo meu grupo

--- murilo.js
+++ murilo.js
@@ -1,6 +1,6 @@
 let pessoa = {
-    nome: "Murilo",
-    passatempo: "Brincar com facas",
-    esporte: "Natação",
-    membrosDoGrupo: ["Alessandro", "Élon", "Murilo", "Paulo"]
-}
+  nome: "Murilo",
+  passatempo: "Brincar com facas",
+  esporte: "Natação",
+  membrosDoGrupo: ["Alessandro", "Élon", "Murilo", "Paulo"]
+};
--- paulo.js
+++ paulo.js
@@ -1,6 +1,6 @@
 let paulo = {
-    Nome = "Paulo Farah Alves",
-    Passatempo = "You Tube",
-    Esporte = "Futebol",
-    MembrosDoGrupo = ["Elon","Murilo","Paulo","Alessandro"]
+  Nome: "Paulo Farah Alves",
+  Passatempo: "You Tube",
+  Esporte: "Futebol",
+  MembrosDoGrupo: ["Elon", "Murilo", "Paulo", "Alessandro"]
 };
